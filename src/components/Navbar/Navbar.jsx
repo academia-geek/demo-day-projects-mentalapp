1109a9c859bea5ddb3ccb6f11f33b1c0ab261fb3
cristiancsandoval <clinares668@gmail.com>
2022-03-22 10:52:02 -0500

    navbar desktop

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { logo } from '../../media/imagenes'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+        <div>
+            <img src={logo} alt='logo MentalApp'/>
+            <h1>MentalApp</h1>
+        </div>
+        <nav>
+          <div>
+            <h4>Servicios</h4>
+            <ul className='desplegable servicios'>
+              <li>Chat con un profesional</li>
+              <li>Agendar una cita con un profesional</li>
+              <li>Visitar foro</li>
+            </ul>
+          </div>
+          <div>
+            <h4>Sobre nosotros</h4>
+          </div>
+          <div>
+            <h4>Perfil</h4>
+            <ul className='desplegable perfil'>
+              <li>Editar perfil</li>
+              <li>Cerrar sesión</li>
+            </ul>
+          </div>
+        </nav>
+    </header>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar/Navbar";
 import './style/css/index.css';
 
 ReactDOM.render(
-   <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+   <React.StrictMode>
+      <Navbar/>   
+   </React.StrictMode>,
    document.getElementById("root")
 );
 
