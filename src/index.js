5a3abe1fab03585ee6436d86ed3100ac5bda721e
felipe22570 <piedra22570@gmail.com>
2022-03-21 22:47:32 -0500

    agregando diseño de login y configuración de firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './style/css/index.css';
+import Login from "./components/Login/Login";
+import "./style/css/index.css";
 
 ReactDOM.render(
-   <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+   <React.StrictMode>
+      <Login />
+   </React.StrictMode>,
    document.getElementById("root")
 );
 
--- src/media/imagenes.js
+++ src/media/imagenes.js
@@ -1,1 +1,6 @@
 export const logo = "";
+
+export const logoGoogle =
+   "https://res.cloudinary.com/dcane9asx/image/upload/v1646412788/5847f9cbcef1014c0b5e48c8_ahn6z7.png";
+export const logoFacebook =
+   "https://res.cloudinary.com/dcane9asx/image/upload/v1646522028/facebook_py8qjc.png";
