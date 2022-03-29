94d8e13aaa6eaf1575da885d700ffbbc1b3e6733
cristiancsandoval <clinares668@gmail.com>
2022-03-28 20:52:59 -0500

    bienvenida inicio

--- src/components/Inicio/Bienvenida.jsx
+++ src/components/Inicio/Bienvenida.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { illustracionHome } from '../../media/imagenes'
 
 const Bienvenida = () => {
   return (
-    <div>Bienvenida</div>
+    <section className='bienvenida'>
+        <div>
+            <img src={illustracionHome} alt='ilustración home'/>
+        </div>
+        <div>
+            <h3>¡Te damos la bienvenida a MentalApp!</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi vero provident saepe voluptatem omnis.</p>
+            <div>
+              <button>Visitar foro</button>
+              <button>Hablar con un profesional</button>
+            </div>
+        </div>
+    </section>
   )
 }
 
